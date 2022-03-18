445d5035c2c60c1f6ed3c833c8b647240ed1a63b
Mduduzi Mthethwa <mduduzimth@gmail.com>
2022-03-18 12:21:53 +0200

    init commit

---
+++ AutoFill/Controllers/OTPViewController.swift
@@ -0,0 +1,31 @@
+//
+//  OTPViewController.swift
+//  AutoFill
+//
+//  Created by mduduzi mthethwa on 2022/03/17.
+//
+
+import UIKit
+
+class OTPViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Profile Verification"
+        tabBarController?.tabBar.barTintColor = UIColor.brown
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
